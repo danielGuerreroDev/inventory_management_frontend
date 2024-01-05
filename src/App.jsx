5c43650bc9f0c29cec7f7b0f3fbfa17172b20af6
danoGuerrero <danielguerrerodev@gmail.com>
2024-01-04 19:06:38 -0600

    routes/pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
-import Menu from "./components/navbar";
-import DataTable from "./components/table";
+import { Route, Routes } from "react-router";
+import Home from "./pages/Home";
+import Menu from "./components/Menu";
+import Products from "./pages/Products";
 
 function App() {
+  const routes = [
+    {
+      path: "/",
+      exact: false,
+      page: null,
+    },
+    {
+      path: "/pages/Home",
+      exact: false,
+      page: <Home />,
+    },
+    {
+      path: "/pages/Products",
+      exact: false,
+      page: <Products />,
+    },
+  ];
+
   return (
     <>
-    <Menu />
-    <DataTable />
+      <Menu />
+      <Routes>
+        {routes.map((route, index) => (
+          <Route
+            element={route.page}
+            exact={route.exact}
+            key={index}
+            path={route.path}
+          />
+        ))}
+      </Routes>
     </>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+function Home() {
+  return (
+    <h1>Hola</h1>
+  );
+}
+
+export default Home;
