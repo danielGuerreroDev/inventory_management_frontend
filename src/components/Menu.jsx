214f8c3a09789533b40af9873aa17ed2d10fdad1
danoGuerrero <danielguerrerodev@gmail.com>
2024-02-22 15:59:37 -0600

    menu highlight active

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,59 @@
-import { Link } from "react-router-dom";
+import { black, green } from "../general/colors"
 import Container from "react-bootstrap/Container";
+import { Link } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { useState } from "react";
 
 function MainMenu() {
+  const [active, setActive] = useState('Stock');
+
+  const styles = {
+    link: {
+      color: black,
+      textDecoration: 'none',
+    },
+    activeLink: {
+      color: green,
+      textDecoration: 'underline',
+    }
+  };
+
+  const menuItems = [
+    {
+      id: 1,
+      to: '/pages/Insights',
+      text: 'Insights'
+    },
+    {
+      id: 2,
+      to: '/pages/Stock',
+      text: 'Stock'
+    },
+    {
+      id: 3,
+      to: '/pages/Sell',
+      text: 'Sell'
+    }
+  ];
+
+  const currentPage = (event) => {
+    setActive(event.target.text);
+  };
+
+  const links = menuItems.map((item) => {
+    return(
+      <Link
+        key={item.id}
+        onClick={currentPage}
+        style={item.text == active ? styles.activeLink : styles.link}
+        to={item.to}
+      >
+        {item.text}
+      </Link>
+    );
+  });
+
   return (
     <Navbar className="bg-body-tertiary" sticky="top">
       <Container className="d-flex justify-content-start gap-5">
@@ -17,9 +67,7 @@ function MainMenu() {
           />
         </Navbar>
         <Nav className="d-flex flex-row gap-3">
-          <Link to="/pages/Insights">INSIGHTS</Link>
-          <Link to="/pages/Stock">STOCK</Link>
-          <Link to="/pages/Sell">SELL</Link>
+          {links}
         </Nav>
       </Container>
     </Navbar>
