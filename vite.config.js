4c2d66a68a0d555827bddf66da1951e1da0a0f70
danoGuerrero <danielguerrerodev@gmail.com>
2024-03-08 14:03:22 -0600

    rewrite url

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const rewriteSlashToIndexHtml = () => {
+  const urls = ['/pages/Insights', '/pages/Stock', '/pages/Sell'];
+
+  return {
+    name: 'rewrite-slash-to-index-html',
+    apply: 'serve',
+    enforce: 'post',
+    configureServer(server) {
+      server.middlewares.use('/', (req, _, next) => {
+        if (urls.some((v) => v === req.url)) {
+          req.url = '/index.html'
+        }
+        next()
+      })
+    },
+  }
+}
+
 export default defineConfig({
   appType: 'spa',
-  plugins: [react()],
+  plugins: [react(), rewriteSlashToIndexHtml()],
 })
